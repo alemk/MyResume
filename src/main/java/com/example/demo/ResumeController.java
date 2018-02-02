1eb1d526db1dd7034a72cf8b95de2f10b34b9f9c
student <student@GBTC441J05LX.montgomerycollege.edu>
2018-02-02 15:02:18 -0500

    friday commit 2

--- src/main/java/com/example/demo/ResumeController.java
+++ src/main/java/com/example/demo/ResumeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,4 +34,24 @@ public class ResumeController {
         return "redirect:/";
     }
 
+   /*@GetMapping("/addskills")
+    public String addSkill(Model model)
+    {
+        model.addAttribute("resume", new Resume());
+
+        return "addskills";
+    }
+
+    @PostMapping("/addskills")
+    public String addEducation(@Valid Resume resume , BindingResult result){
+
+        if(result.hasErrors()){
+            return "addresume";
+        }
+
+        resumeRepository.save(resume);
+
+        return "redirect:/";
+    }
+*/
 }
